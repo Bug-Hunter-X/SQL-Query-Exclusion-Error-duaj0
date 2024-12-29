c970d750b5519da66306e7702bf2d35f8a9aa0ad
BugHunter-X <askzenai@gmail.com>
2024-12-29 14:04:03 +0100

    Add bugSolution.sql file

---
+++ bugSolution.sql
@@ -0,0 +1,5 @@
+```sql
+SELECT * FROM employees WHERE department = 'Sales' AND salary >= 100000;
+```
+
+This corrected query uses the `>=` operator, ensuring that employees with a salary of 100000 are also included in the results.
